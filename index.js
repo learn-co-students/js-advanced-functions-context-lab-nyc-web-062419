ead8e1bd54a58c17f7a609d08a1b14d620c481dd
ghatzid <ghatzid@gmail.com>
2019-08-15 14:33:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,91 @@
-/* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecord(bullshit){
+  return {
+    firstName: bullshit[0],
+    familyName: bullshit[1],
+    title: bullshit[2],
+    payPerHour: bullshit[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployees(aPileOfBullshit)
+{
+  return aPileOfBullshit.map(bullshit => {
+    return createEmployeeRecord(bullshit)
+  });
+}
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
+function createTimeInEvent(dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+    return this
 }
+
+function createTimeOutEvent(dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(soughtDate){
+  let inEvent = this.timeInEvents.find(function(e){
+    return e.date === soughtDate
+})
+
+let outEvent = this.timeOutEvents.find(function(e){
+    return e.date === soughtDate
+})
+return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+
+
+function wagesEarnedOnDate(soughtDate){
+return hoursWorkedOnDate.call(this , soughtDate) * this.payPerHour
+}
+
+let allWagesFor = function(){
+  let eligibleDates = this.timeInEvents.map(function(e){
+      return e.date
+  })
+
+  let payable = eligibleDates.reduce(function(memo, d){
+      return memo + wagesEarnedOnDate.call(this, d)
+  }.bind(this), 0)
+
+  return payable
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+  return arrayOfEmployeeRecords.reduce(function(memo, rec){
+      return memo + allWagesFor.call(rec)
+  }, 0)
+}
+
+let createEmployeeRecords = function(src) {
+  return src.map(function(row){
+    return createEmployeeRecord(row)
+  })
+}
+
+let findEmployeebyFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
